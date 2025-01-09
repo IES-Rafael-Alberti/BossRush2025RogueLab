5b3f62f027e45a8168f6744cd3d0966a7f5ebc5e
DriveSP <gtavvice@hotmail.es>
2025-01-09 11:13:07 +0100

    Arrows
    Destroy arrows when key down

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -1,9 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
 
     public bool canBePressed;
+
+    public KeyCode keyPressed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +16,13 @@ public class NoteObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(keyPressed))
+        {
+            if (canBePressed)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
