36279b122a1711c7aa7dc811bf8f84bc668adcd5
DriveSP <gtavvice@hotmail.es>
2025-01-11 10:19:29 +0100

    GameManager
    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    [SerializeField] private int score;
+    [SerializeField] private int health;
+
+
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region Health
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void SetHealth(int n, string restore)
+    {
+        Debug.Log("GM: Health changes");
+        switch (n)
+        {
+            case 1:
+                if (restore == null)
+                {
+                    Debug.Log("GM: Health removed");
+                    health -= 1;
+                }
+                else
+                {
+                    Debug.Log("GM: Health restored");
+                    health += 1;
+                }
+                break;
+            case 9:
+                Debug.Log("GM: Health completly restored");
+                health = 9; break;
+        }
+    }
+    #endregion
+}
